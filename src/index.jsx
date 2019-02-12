0d1949f5f6ae2fe9f0e6b1379e6056301ce9e094
renzhibin <zhibin.ren@colorv.com>
2019-02-12 17:50:49 +0800

    初始化

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,11 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 import * as serviceWorker from "./serviceWorker"
-import App from "./containers/App"
 import RouterMap from "./router/RouterMap"
 import {hashHistory} from "react-router"
 
 
-ReactDOM.render(<RouterMap history={hashHistory}/>, document.getElementById("root"))
+ReactDOM.render((<RouterMap history={hashHistory}/>), document.getElementById("root"))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
